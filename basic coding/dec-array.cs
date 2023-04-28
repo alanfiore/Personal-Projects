81a2614040662bcedb4eab7a69e667787aefb145
alanfiore <alanfiore7@gmail.com>
2023-04-27 21:55:13 -0400

    tailwind

---
+++ basic coding/dec-array.cs
@@ -0,0 +1,18 @@
+
+
+int[]nums = new int[10]; 
+
+// int = type, [] = array, nums = name, 10 = how long the array is 
+    // first half is step 1 (declare) and second half is 1a ("create)
+
+nums[4] = 132;
+nums[0] = -3;
+
+point(nums[1], nums[2])
+
+// step 2 is initialize, 10 spots and index values range from 0-9
+// this sets 4 to 132, or the 5th spot to 132
+
+
+
+
