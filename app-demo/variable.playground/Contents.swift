99bf9fdee279dd6f5271714005a34ba8d1a604d2
alanfiore <alanfiore7@gmail.com>
2023-05-01 18:31:41 -0400

    more app work

---
+++ app-demo/variable.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting:String = "Hello, playground"
+
+
+
+
+greeting = "I can write Swift"
+
+print(greeting)
+
+
+var myInt:Int = 5
+print(myInt)
+
+var myDouble:Double = 0.5
+print(myDouble)
+
+var myBool:Bool = false
+print(myBool)
